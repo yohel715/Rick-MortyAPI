c010765c7462301021793502ae3a77688fa409bc
yohel715 <yohel715@gmail.com>
2020-09-25 14:44:03 -0600

    adapter

--- app/src/main/java/cr/ac/ucr/rickandmortyapi/adapters/CharactersAdapter.java
+++ app/src/main/java/cr/ac/ucr/rickandmortyapi/adapters/CharactersAdapter.java
@@ -1,17 +1,39 @@
 package cr.ac.ucr.rickandmortyapi.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
+import cr.ac.ucr.rickandmortyapi.R;
+
 public class CharactersAdapter extends RecyclerView.Adapter<CharactersAdapter.ViewHolder> {
+
+    private Context context;
+    private ArrayList<Character> characters;
+
+    public CharactersAdapter(Context context, ArrayList<Character> characters) {
+        this.context = context;
+        this.characters = characters;
+    }
+
+    public CharactersAdapter(Context context) {
+        this.context = context;
+        this.characters = new ArrayList<>();
+    }
 
     @NonNull
     @Override
     public CharactersAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.character, parent, false);
+
+        return new ViewHolder(view);
     }
 
     @Override
